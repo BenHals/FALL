4678d5409f83fa783914d0d8a147de9b41948d9b
ben <benhalstd@gmail.com>
2022-03-29 16:13:25 +1300

    tests for error rate representation

--- streamselect/concept_representations/error_rate_representation.py
+++ streamselect/concept_representations/error_rate_representation.py
@@ -5,7 +5,7 @@ from river.stats import RollingMean
 
 from streamselect.concept_representations import ConceptRepresentation
 
-from .meta_feature_distributions import DistributionTypes, SingleValueDistribution
+from .meta_feature_distributions import SingleValueDistribution
 
 
 class ErrorRateRepresentation(ConceptRepresentation):
@@ -23,11 +23,3 @@ class ErrorRateRepresentation(ConceptRepresentation):
         avg_error_rate = self.recent_error_rate.get()
         self.values[0] = avg_error_rate
         self.distribution[0].learn_one(avg_error_rate)
-
-    @property
-    def _vector(self) -> bool:
-        return False
-
-    @property
-    def _distribution(self) -> DistributionTypes:
-        return DistributionTypes.NA
