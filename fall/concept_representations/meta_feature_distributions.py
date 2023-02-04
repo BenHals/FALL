3f7ffb8eb045ee22acb971baa0e5f9fe40a1f46f
ben <benhalstd@gmail.com>
2023-02-04 14:30:08 +1300

    Add count to distributions

--- fall/concept_representations/meta_feature_distributions.py
+++ fall/concept_representations/meta_feature_distributions.py
@@ -5,7 +5,7 @@ from enum import Enum
 from math import sqrt
 
 from river import utils
-from river.stats import Max, Mean, Min, Var
+from river.stats import Count, Max, Mean, Min, Var
 
 
 class DistributionTypes(Enum):
@@ -20,12 +20,23 @@ class DistributionTypes(Enum):
 class BaseDistribution(abc.ABC):
     """Represents the distribution of a meta-feature across a concept."""
 
-    @abc.abstractmethod
+    def __init__(self, memory_size: int = -1) -> None:
+        self.memory_size = memory_size
+        self.is_rolling = self.memory_size > 0
+        self.count_stat = Count()
+
     def learn_one(self, val: float) -> None:
         """Add a new value to the distribution."""
+        self.count_stat.update(1)
 
     def reset_distribution(self) -> None:
         """Reset the variance associated with the meta-feature."""
+
+    @property
+    def count(self) -> int:
+        """Returns a value describing the distribution."""
+        c = self.count_stat.get()
+        return min(c, self.memory_size) if self.is_rolling else c
 
     @property
     def mean(self) -> float:
@@ -56,12 +67,14 @@ class SingleValueDistribution(BaseDistribution):
     """A distribution containing only the most recent value."""
 
     def __init__(self, memory_size: int = -1) -> None:
+        super().__init__(memory_size)
         self.distribution_type = DistributionTypes.NA
         self.value: float = 0
         self.max_stat = Max()
         self.min_stat = Min()
 
     def learn_one(self, val: float) -> None:
+        super().learn_one(val)
         self.value = val
         self.max_stat.update(val)
         self.min_stat.update(val)
@@ -85,9 +98,8 @@ class GaussianDistribution(BaseDistribution):
     """A distribution containing the mean and standard deviation of values."""
 
     def __init__(self, memory_size: int = -1) -> None:
-        self.memory_size = memory_size
+        super().__init__(memory_size)
         self.distribution_type = DistributionTypes.GAUSSIAN
-        self.is_rolling = self.memory_size > 0
         if self.is_rolling:
             self.var = utils.Rolling(Var(), window_size=memory_size)
             self.mean_stat = utils.Rolling(Mean(), window_size=memory_size)
@@ -100,6 +112,7 @@ class GaussianDistribution(BaseDistribution):
         self.min_stat = Min()
 
     def learn_one(self, val: float) -> None:
+        super().learn_one(val)
         self.var.update(val)
         self.mean_stat.update(val)
         self.max_stat.update(val)
